4be26edda2372d2170a02a4c34f8dd97fe1600e6
Dandi <dandi.juliandi@nawadata.id>
2025-02-01 20:15:52 +0700

    chang text to longText in products table

--- database/migrations/2025_01_05_102542_create_products_table.php
+++ database/migrations/2025_01_05_102542_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->text('image')->nullable();
+            $table->longText('image')->nullable();
             $table->string('name');
             $table->string('size');
             $table->integer('stock');
