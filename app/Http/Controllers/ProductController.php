f8afa94d894b79067f38506eb68cf8177ca9ffb7
Dandi Juliandi <dandi.juliandi@nawadata.id>
2025-02-25 13:46:43 +0700

    add category property in Detail Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -52,7 +52,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        return $product;
+        return $product->with('category');
     }
 
     /**
