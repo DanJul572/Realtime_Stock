fcd72f9b93a425ff3971001b1fb027d7f440752a
Dandi <dandi.juliandi@nawadata.id>
2025-02-25 19:54:14 +0700

    fix error in detail product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -52,7 +52,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        return $product->with('category');
+        return $product->load('category');
     }
 
     /**
